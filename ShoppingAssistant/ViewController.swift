c705a1ed7567489c36b63e1284ec0d53875e47a6
rares <rares_gamanrares@yahoo.com>
2019-10-02 23:12:34 +0300

    Initial Commit

---
+++ ShoppingAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingAssistant
+//
+//  Created by Gaman Rares-Constantin on 10/2/19.
+//  Copyright © 2019 Gaman Rares-Constantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
