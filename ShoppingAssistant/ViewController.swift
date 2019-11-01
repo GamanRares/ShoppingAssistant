ecef22275758657c362b2f726497865566cefc43
rares <rares_gamanrares@yahoo.com>
2019-11-01 23:36:03 +0200

    Demo login page

--- ShoppingAssistant/ViewController.swift
+++ ShoppingAssistant/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func Login(_ sender: Any) {
+        for user in HighPerformanceButCostlyDatabase.users {
+            if user.email == usernameField.text! {
+                let utils = Utils()
+                if user.password == utils.sha256(stringToEncrypt: passwordField.text!){
+                    performSegue(withIdentifier: "LoginSegue", sender: self)
+                }
+            }
+        }
+        
+        let alert = UIAlertController(title: "Error", message: "Username or password are incorect", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Try again", style: .default, handler: nil))
+        self.present(alert, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        HighPerformanceButCostlyDatabase.users.insert(User(fullName: "test", password: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", email: "test@yahoo.com", role: Role.ADMINISTRATOR))
+    
     }
-
 
 }
 
